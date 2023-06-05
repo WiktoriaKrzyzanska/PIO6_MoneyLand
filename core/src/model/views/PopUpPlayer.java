e5edb48cd4e135dac26b100e70cf224d2778dfef
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2023-06-05 20:25:38 +0200

    2.Working pop up but no working button

--- core/src/model/views/PopUpPlayer.java
+++ core/src/model/views/PopUpPlayer.java
@@ -1,57 +1,80 @@
 package model.views;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.*;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.mygdx.game.MoneyLandGame;
 
-public class PopUpPlayer extends Table {
-    String title;
-    ImageButton startButton;
+public class PopUpPlayer extends Group {
+
     private final ShapeRenderer shapeRenderer;
     private final SpriteBatch spriteBatch;
     private final BitmapFont font;
+    private ImageButton startButton;
+    String text;
+    Stage stage;
 
-        public PopUpPlayer(String title, ShapeRenderer shapeRenderer) {
-            // Create a label for the title
-            this.title = title;
-            this.shapeRenderer = shapeRenderer;
-            font = new BitmapFont();
-            spriteBatch = new SpriteBatch();
-            Texture buttonTexture = new Texture("StartButton.png");
-            Texture buttonHoverTexture = new Texture("StartButtonClicked.png");
+    public PopUpPlayer(ShapeRenderer shapeRenderer, String text) {
+        this.shapeRenderer = shapeRenderer;
+        this.text = text;
+        stage = new Stage();
+        font = new BitmapFont();
+        spriteBatch = new SpriteBatch();
+        setVisible(true);
+        Texture buttonTexture = new Texture("StartButton.png");
+        Texture buttonHoverTexture = new Texture("StartButtonClicked.png");
 
-            ImageButton.ImageButtonStyle buttonStyle = new ImageButton.ImageButtonStyle();
-            buttonStyle.up = new TextureRegionDrawable(new TextureRegion(buttonTexture));
-            buttonStyle.over = new TextureRegionDrawable(new TextureRegion(buttonHoverTexture));
+        ImageButton.ImageButtonStyle buttonStyle = new ImageButton.ImageButtonStyle();
+        buttonStyle.up = new TextureRegionDrawable(new TextureRegion(buttonTexture));
+        buttonStyle.over = new TextureRegionDrawable(new TextureRegion(buttonHoverTexture));
 
-            startButton = new ImageButton(buttonStyle);
+        startButton = new ImageButton(buttonStyle);
+        startButton.setPosition(1000,1000);
+        startButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                setVisible(false);
+            }
 
-            // Create a text button for the exit
-            startButton.addListener(new ClickListener() {
-                @Override
-                public void clicked(InputEvent event, float x, float y) {
-                    setVisible(false); // hide the table when exit button is clicked
-                }
-            });
+        });
 
-            // Add the title and the exit button to the table
-            add(startButton).width(100).height(50); // set the size of the second cell
-            row(); // start a new row
+        stage.addActor(startButton);
+    }
 
-            // Add any other actors or content to the table as needed
-            // For example, you can add an image of your player pawn
-
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        if (isVisible()) {
+            shapeRenderer.setProjectionMatrix(getStage().getCamera().combined);
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            shapeRenderer.setColor(244/255f, 255/255f, 175/255f,1);
+            shapeRenderer.rect(getX(), getY(), getWidth(), getHeight());
+            shapeRenderer.end();
+            spriteBatch.begin();
+            font.setColor(Color.BLACK);
+            font.getData().setScale(2, 2);
+            font.draw(spriteBatch, text, getX() , getY() + 300);
+            spriteBatch.end();
         }
     }
 
-
+    public void dispose() {
+        spriteBatch.dispose();
+        font.dispose();
+    }
+}
 
 
 
