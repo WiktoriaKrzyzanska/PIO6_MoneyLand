ce98daf757a526316c867e592e260cf3f174e884
Jul <jtrzebuchowska1@gmail.com>
2023-06-13 10:53:22 +0200

    COL-25 added information popUp

--- core/src/model/views/GameScreen.java
+++ core/src/model/views/GameScreen.java
@@ -61,6 +61,7 @@ public class GameScreen extends Shortcut {
     PopUpInformation popUpRules;
     PopUpInformation popUpMoney;
     PopUpInformation popUpFirstPlayer;
+    PopUpInformation popUpTrapCard;
     PopUpInformation popUpZgierz;
     ImageButton startButton;
 
@@ -97,7 +98,7 @@ public class GameScreen extends Shortcut {
             case 4:pawnName="LECH";break;
         }
         //set popups when game start
-        String Welcome = "Czesc " + parent.getPlayer().getPlayerName() + "! Twój pionek to "+pawnName;
+        String Welcome = "Czesc " + parent.getPlayer().getPlayerName() + "! Twoj pionek to "+pawnName;
         String Rules = "Zasady";
         String Cebulion = "Na poczatek gry dostajesz 500 cebulionow." +
                 "Wydawaj je madrze";
@@ -118,14 +119,15 @@ public class GameScreen extends Shortcut {
         popUpMoney = new PopUpInformation(Cebulion, true);
         popUpRules = new PopUpInformation( Rules, true);
         popUpPlayer = new PopUpInformation( Welcome, true);
-
-        popUpZgierz=new PopUpInformation("Ups! Kupiłeś Zgierz :( przygotuj się na straty",false);
+        popUpTrapCard=new PopUpInformation("Jedno pole dziala inaczej niz pozostale ;)",true);
+        popUpZgierz=new PopUpInformation("Ups! Kupiles Zgierz :( przygotuj sie na straty",false);
 
         popUpFirstPlayer.setVisible(true);
         popUpPlayer.setVisible(true);
         popUpRules.setVisible(true);
-        popUpMoney.setVisible(true);
 
+        popUpMoney.setVisible(true);
+        popUpTrapCard.setVisible(true);
         //create part with cards
         cardsManager = new CardsManager(boardWidth,  boardHeight, leftSideWidth, MoneyLandGame.HEIGHT/6);
 
@@ -165,13 +167,17 @@ public class GameScreen extends Shortcut {
         popUpRules.setSize(MoneyLandGame.WIDTH/2, MoneyLandGame.HEIGHT/2);
         popUpPlayer.setPosition(MoneyLandGame.WIDTH/4, MoneyLandGame.HEIGHT/4);
         popUpPlayer.setSize(MoneyLandGame.WIDTH/2, MoneyLandGame.HEIGHT/2);
-
+        popUpTrapCard.setPosition(MoneyLandGame.WIDTH/4, MoneyLandGame.HEIGHT/4);
+        popUpTrapCard.setSize(MoneyLandGame.WIDTH/2, MoneyLandGame.HEIGHT/2);
         popUpZgierz.setPosition(MoneyLandGame.WIDTH/4, MoneyLandGame.HEIGHT/4);
         popUpZgierz.setSize(MoneyLandGame.WIDTH/2, MoneyLandGame.HEIGHT/2);
         stage2.addActor(popUpZgierz);
+
         stage2.addActor(popUpFirstPlayer);
+        stage2.addActor(popUpTrapCard);
         stage2.addActor(popUpMoney);
         stage2.addActor(popUpRules);
+
         stage2.addActor (popUpPlayer);
 
         stage.addActor(menuButton);
